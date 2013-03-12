eca7d025d88ca5540058da41f7de7cfa208ec931
hafez <hafez@gatech.edu>
2013-03-11 21:36:25 -0400

    initial commit

---
+++ apidocs/api.js
@@ -0,0 +1,31 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ARC",
+        "ARGON",
+        "ARObject",
+        "Channel",
+        "ChannelView",
+        "FrameMarkerTarget",
+        "GeoLocation",
+        "GeoObject",
+        "GeoSpot",
+        "GeoSpotPanorama",
+        "TrackedObject",
+        "TrackerManager",
+        "TrackingTarget",
+        "Transform3D",
+        "VuforiaDataSet",
+        "VuforiaImageTarget"
+    ],
+    "modules": [
+        "ARGON"
+    ],
+    "allModules": [
+        {
+            "displayName": "ARGON",
+            "name": "ARGON"
+        }
+    ]
+} };
+});
